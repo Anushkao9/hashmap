c8720fc5db7dea9c03e4e2a0f20ffb14e173adc8
Anushka Saurabh <95422932+Anushkao9@users.noreply.github.com>
2022-01-29 17:19:18 +0530

    Update Loop.java

--- even number/Loop.java
+++ even number/Loop.java
@@ -1,7 +1,10 @@
-public class Loop{
-public static void main(String[] args){
-for(int i=1; i<=100; i=i+2){
-System.out.println(i);
-}
-}
+public class Loop
+{
+  public static void main(String[] args)
+  {
+    for(int i=1; i<=100; i=i+2)
+    {
+        System.out.println(i);
+    }
+  }
 }
